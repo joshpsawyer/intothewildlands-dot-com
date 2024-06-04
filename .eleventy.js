2131f1b00b878057476a8bb325db48de68ec66c3
Josh P. Sawyer <josh.p.sawyer@gmail.com>
2024-06-04 14:45:04 -0400

    added CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("CNAME");
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addWatchTarget("./src/css/");
